12e02e0c71f5fde8e04ebaf877eb45d6bdb2f115
Nicholas Lingad <lingadnicholas@engineering.ucla.edu>
2022-06-24 10:48:42 -0700

    Added reviews

---
+++ script.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = ''; // INSERT API KEY HERE
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [34.19316474737605, -119.18824244252498],
+zoom: 18,
+});
